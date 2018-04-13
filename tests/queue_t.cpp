7739b82752a6b67d9fb69255f9c7120d0c856646
Dzhabrail Elmezov <elmezov007@gmail.com>
2018-04-13 21:21:55 +0300

    Update queue_t.cpp

--- tests/queue_t.cpp
+++ tests/queue_t.cpp
@@ -1,11 +1,36 @@
 #include <catch.hpp>
 #include <sstream>
-#include <cassert>
-#include "queue.hpp"
 
-TEST_CASE("creating queue")
+#include "queue1.cpp"
+
+TEST_CASE("pop&push")
 {
-	queue_t<int> q;
-	REQUIRE( vector.size() == 0 );
-	REQUIRE( vector.capacity() == 0 );
+    queue_t<int> q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    
+   
+    std::ostringstream ostream;
+    ostream<<q.pop();
+    q.pop();
+    q.pop();
+    
+    REQUIRE_THROWS(q.pop()); 
+    
+    REQUIRE( "1" == ostream.str() );
 }
+TEST_CASE("copy")
+{
+    queue_t<int> q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    
+   
+    std::ostringstream ostream;
+    queue_t<int> q1=q;
+    
+    ostream<<q1.pop();
+    
+    REQUI
