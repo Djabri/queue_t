329a96ce63622e45405f22f6d661dc1a87c8c402
Dzhabrail Elmezov <elmezov007@gmail.com>
2018-04-13 21:30:55 +0300

    Update queue_t.cpp

--- tests/queue_t.cpp
+++ tests/queue_t.cpp
@@ -3,35 +3,7 @@
 
 #include "queue1.cpp"
 
-TEST_CASE("pop&push")
+TEST_CASE("pop")
 {
-    queue_t<int> q;
-    q.push(1);
-    q.push(2);
-    q.push(3);
-    
-   
-    std::ostringstream ostream;
-    ostream<<q.pop();
-    q.pop();
-    q.pop();
-    
-    REQUIRE_THROWS(q.pop()); 
-    
-    REQUIRE( "1" == ostream.str() );
-}
-TEST_CASE("copy")
-{
-    queue_t<int> q;
-    q.push(1);
-    q.push(2);
-    q.push(3);
-    
-   
-    std::ostringstream ostream;
-    queue_t<int> q1=q;
-    
-    ostream<<q1.pop();
-    
-    REQUIRE( "1" == ostream.str() );
+
 }
